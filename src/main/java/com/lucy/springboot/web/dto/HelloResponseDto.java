6dbf2cc22fe9e870a04ff7a2d69a338f9fe11457
cindybohyeon <bohyeoncindy@gmail.com>
2024-01-16 12:27:37 +0900

    feat : controller, dto 연습

---
+++ src/main/java/com/lucy/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,9 @@
+package com.lucy.springboot.web.dto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+  private final String name;
+  private final int amount;
+}
