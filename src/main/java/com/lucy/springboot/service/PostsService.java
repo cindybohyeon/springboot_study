16c4681bc9697994e4c5770a02afb9b54fcebc28
cindybohyeon <bohyeoncindy@gmail.com>
2024-01-23 15:54:37 +0900

    feat : mustate로 화면 + API 연습

--- src/main/java/com/lucy/springboot/service/PostsService.java
+++ src/main/java/com/lucy/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.lucy.springboot.service;
 
 import com.lucy.springboot.domain.posts.Posts;
 import com.lucy.springboot.domain.posts.PostsRepository;
+import com.lucy.springboot.web.dto.PostsListResponseDto;
 import com.lucy.springboot.web.dto.PostsResponseDto;
 import com.lucy.springboot.web.dto.PostsSaveRequestDto;
 import com.lucy.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,6 +37,19 @@ public class PostsService {
     Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다, id =" + id));
 
     return new PostsResponseDto(entity);
+  }
+
+  @Transactional(readOnly = true)
+  public List<PostsListResponseDto> findAllDesc() {
+    return postsRepository.findAllDesc().stream()
+      .map(PostsListResponseDto::new)
+      .collect(Collectors.toList());
+  }
+
+  @Transactional
+  public void delete (Long id) {
+    Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+    postsRepository.delete(posts);
   }
 
 }
